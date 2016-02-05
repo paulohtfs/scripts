7ba00cfb3166bf66de07ace1faa7078f507913fb
Paulo Tada <paulohtfs@gmail.com>
2016-02-04 23:25:35 -0200

    Addin recipes [unpackaged,utils] cookbook [packages]

---
+++ cookbooks/packages/recipes/unpackaged.rb
@@ -0,0 +1,7 @@
+# This is a recipe that install tools from a non official
+#   debian package repository. Some of the tools are deb
+#   package made by the developers and may contain unknown bugs
+
+# Atom:
+#   Text editor
+
---
+++ cookbooks/packages/recipes/utils.rb
@@ -0,0 +1,18 @@
+# The following packages are used on a normal
+#   day-to-day work environment. Add the package
+#   that are used on Debian system.
+#   
+# This may not work on other dist
+
+# Basics tools
+package 'xclip'
+package 'vim'
+package 'tmux'
+package 'tree'
+
+# Development tools
+package 'git'
+package 'virtualbox-5.0'
+
+# Comunication tools
+package 'irssi'
