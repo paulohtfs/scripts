16887eea767141e07908cbbeff7059aab37207cf
Paulo Tada <paulohtfs@gmail.com>
2016-02-05 23:50:56 -0200

    Adding atom to unpackaged recipe

--- cookbooks/packages/recipes/unpackaged.rb
+++ cookbooks/packages/recipes/unpackaged.rb
@@ -4,4 +4,13 @@
 
 # Atom:
 #   Text editor
+ATOM_DEB_DIR='/tmp/atom-amd64.deb'
+remote_file ATOM_DEB_DIR do
+  source 'https://atom.io/download/deb'
+  owner 'root'
+  group 'root'
+end
 
+dpkg_package 'atom' do
+  source ATOM_DEB_DIR
+end
