26117d4e04e7ef2b7d7bb30609a71ab5e3c03d64
Paulo Tada <paulohtfs@gmail.com>
2016-02-06 16:51:29 -0200

    Adding new cookbook chef-workstation

---
+++ cookbooks/chef-workstation/recipes/default.rb
@@ -0,0 +1,10 @@
+# This cookbook will install some of the main tools
+#   for chef cookbook creaters. You may want to reduce
+#   the amount of tools if you need.
+
+# Some lib deps for gems
+package 'zlib1g-dev' # for foodcritic
+
+gem_package 'foodcritic' # lint tool
+gem_package 'chefspec' # unit test
+
