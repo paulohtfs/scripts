3c2371d0898dfd0db3c8215adb582c7e8381236e
Paulo Tada <paulohtfs@gmail.com>
2016-02-10 18:16:23 -0200

    Adding vagrant to packages:unpackaged

--- cookbooks/packages/recipes/unpackaged.rb
+++ cookbooks/packages/recipes/unpackaged.rb
@@ -14,3 +14,17 @@ end
 dpkg_package 'atom' do
   source ATOM_DEB_DIR
 end
+
+# Vagrant:
+#   Development environment
+#
+# This will isntall a debian package of vagrant from the official
+#   website www.vagrantup.com. There is a .deb avable on apt repo
+#   but is not update.
+VAGRANT_DEB_DIR='/tmp/vagrantup.deb'
+remote_file VAGRANT_DEB_DIR do
+  source 'https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb'
+  owner 'root'
+  group 'root'
+end
+
