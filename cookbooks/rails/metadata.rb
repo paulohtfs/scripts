8e6fd3129e0f7f526b564f5206a515a7cf325885
Paulo Tada <paulohtfs@gmail.com>
2016-03-09 10:57:26 -0300

    Updating rails cookbook
    Iproving metadata and creating the gem recipe

--- cookbooks/rails/metadata.rb
+++ cookbooks/rails/metadata.rb
@@ -1,7 +1,7 @@
 name             'rails'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures rails'
+maintainer       'Paulo Tada'
+maintainer_email 'paulohtfs@gmail.com'
+license          'GPL v3'
+description      'Configure the rvm to use a isolated work env'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
---
+++ cookbooks/rails/recipes/gems.rb
@@ -0,0 +1,7 @@
+# TODO: review the usage of gem_package
+# List of gems
+
+#gem_package 'rake'
+#gem_package 'chake'
+#gem_package 'serverspec'
+#gem_package 'chefspec'
